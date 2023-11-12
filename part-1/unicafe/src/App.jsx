29b734551cd72b0614056da0b2f3be7b3ce59560
amuntean <ademdigitbcn@gmail.com>
2023-11-12 22:22:55 +0100

    exercise 1.9

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -12,16 +12,23 @@ const FeedbackButtons = ({setGood, setNeutral, setBad}) => {
 
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
-  return (
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {((good * 1) + (bad * -1) + (neutral * 0)) / total}</p>
-      <p>positive {(good / total)*100} %</p>
-    </>
-  )
+  if (total !== 0) {
+    return (
+      <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {((good * 1) + (bad * -1) + (neutral * 0)) / total}</p>
+        <p>positive {(good / total)*100} %</p>
+      </>
+    )
+  } else {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+ 
 }
 
 const App = () => {
