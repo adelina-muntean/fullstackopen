85fad754a72f8d67e0d8d307f801c9c242475842
amuntean <ademdigitbcn@gmail.com>
2023-11-12 19:21:50 +0100

    refactoring code as in exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,10 +4,12 @@ const Header = ({course}) => {
   )
 }
 
-const Content = ({part, exercises}) => {
+const Content = ({part1, part2, part3}) => {
   return (
     <div>
-      <Part part={part} exercises={exercises}/>
+      <Part part={part1.name} exercises={part1.exercises}/>
+      <Part part={part2.name} exercises={part2.exercises}/>
+      <Part part={part3.name} exercises={part3.exercises}/>
     </div>
   )
 }
@@ -28,20 +30,24 @@ const Total = ({total}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total total={exercises1+exercises2+exercises3} /> 
+      <Content part1={part1} part2={part2} part3={part3}  />
+      <Total total={part1.exercises+part2.exercises+part3.exercises} /> 
     </div>
   )
 }
