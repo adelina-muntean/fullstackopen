ac82031399125b3a312f5b3eaa6b406c20a956b6
adelina-muntean <adelina.muntean97@gmail.com>
2023-11-14 20:27:10 +0100

    exercise 2.1

--- part-2/course-info/src/App.jsx
+++ part-2/course-info/src/App.jsx
@@ -1,35 +1,38 @@
-// const Header = ({course}) => {
-//   return(
-//     <h1>{course}</h1>
-//   )
-// }
-
-// const Content = ({parts}) => {
-//   return (
-//     <div>
-//       <Part part={parts[0].name} exercises={parts[0].exercises}/>
-//       <Part part={parts[1].name} exercises={parts[1].exercises}/>
-//       <Part part={parts[2].name} exercises={parts[2].exercises}/>
-//     </div>
-//   )
-// }
-
-// const Part = ({part, exercises}) => {
-//   return (
-//     <p>
-//       {part} {exercises}
-//     </p>
-//   )
-// }
-
 // const Total = ({parts}) => {
 //   return (
 //     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 //   )
 // }
 
+const Header = ({name}) => {
+  return(
+    <h1>{name}</h1>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.name} part={part} />
+      )}
+    </div>
+  )
+}
+
 const Course = ({course}) => {
-  console.log(course)
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
 }
 
 const App = () => {
